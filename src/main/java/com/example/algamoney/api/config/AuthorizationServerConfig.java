4fa59d368043c2565883666e48510038906a0fdf
Renan Duarte <renandg.axl@gmail.com>
2020-12-28 22:43:01 -0300

    Funcionalidade 32 - Adicionar permissões e escopo nos métodos CRUD.

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -28,11 +28,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	    clients.inMemory()
 	            .withClient("angular")
-	            .secret("$2a$10$G1j5Rf8aEEiGc/AET9BA..xRR.qCpOUzBZoJd8ygbGy6tb3jsMT9G")
+	            .secret("$2a$10$G1j5Rf8aEEiGc/AET9BA..xRR.qCpOUzBZoJd8ygbGy6tb3jsMT9G") // @ngul@r0 - Isto que deve ser digitado no password na aba Authorization do Postman. Lembrando que nesta aba deve ser colocado o Basic Auth no Type
 	            .scopes("read", "write")
 	            .authorizedGrantTypes("password", "refresh_token")
 	            .accessTokenValiditySeconds(1800)
-	            .refreshTokenValiditySeconds(3600 * 24);
+	            .refreshTokenValiditySeconds(3600 * 24)
+				.and()
+				.withClient("mobile")
+				.secret("$2a$10$wciR8q9y8ByH4bffR4j0AOadZIyD8fUzoTa9aW6RvvF78O6eVftX.") // m0b1l30 - Isto que deve ser digitado no password na aba Authorization do Postman. Lembrando que nesta aba deve ser colocado o Basic Auth no Type
+				.scopes("read")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(1800)
+				.refreshTokenValiditySeconds(3600 * 24);
 	}
 	
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
