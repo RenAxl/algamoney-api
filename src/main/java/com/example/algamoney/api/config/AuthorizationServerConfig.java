8780b2ac7023b1d8aed5f4a393e63f81124cd740
Renan Duarte <renandg.axl@gmail.com>
2021-01-14 11:57:35 -0300

    Funcionalidade 38 - Adicionar o nome do usuário no token

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.example.algamoney.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,13 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.example.algamoney.api.config.token.CustomTokenEnhancer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -42,14 +48,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.refreshTokenValiditySeconds(3600 * 24);
 	}
 	
+	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-	    endpoints
-	        .tokenStore(tokenStore())
-	        .accessTokenConverter(this.accessTokenConverter())
-	        .reuseRefreshTokens(false)
-	        .userDetailsService(this.userDetailsService)
-	        .authenticationManager(this.authenticationManager);
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+		
+		endpoints
+			.tokenStore(tokenStore())
+			.tokenEnhancer(tokenEnhancerChain)
+			.reuseRefreshTokens(false)
+			.authenticationManager(authenticationManager);
 	}
+	
 	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
@@ -62,5 +72,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
 	}
+	
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+	    return new CustomTokenEnhancer();
+	}
+	
 	
 }
